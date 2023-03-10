e292af53b00f2ecca8610e0fe65538fd0c8c0dd3
Daniele Battaglioli <108366709+DaniBatta20@users.noreply.github.com>
2023-03-10 11:15:10 +0100

    Create Main.java

---
+++ MazzoDiCarta/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        //creo il mazzo
+        MazzoDiCarte mioMazzo = new MazzoDiCarte();
+        //mischio il mazzo
+        mioMazzo.mischiaMazzo();
+        //mostro il mazzo
+        mioMazzo.mostraMazzo();
+    }
+}
