f9d87673468707528d0247d026e86a860e51cc37
Daniele Battaglioli <108366709+DaniBatta20@users.noreply.github.com>
2023-08-05 16:00:38 +0200

    Update Main.java

--- Estate/PrimaEsercitazione/Main.java
+++ Estate/PrimaEsercitazione/Main.java
@@ -1,1 +1,24 @@
+/******************************************************************************
+Consegna....
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	    //Come si creano gli oggetti?
+	    //(Nome classe) (nome oggetto) = new (chiamata al costruttore);
+	    
+	    //Creo un oggetto p1 usando il costruttore di default:
+	    Prodotto p1 = new Prodotto();
+	    //Creo un oggetto p2 usando il costruttore con parametri:
+	    Prodotto p2 = new Prodotto(1, "Frullatore", "costoso", 150.50, 15);
+	    //Creo un oggetto p3 usando il costruttore di copia con p2:
+	    Prodotto p3 = new Prodotto(p2);
+	    
+	    //Usando il setter, cambiare il prezzo di p3 impostandolo a 190.50:
+	    //Come si invocano i metodi sugli oggetti?
+	    //(nome oggetto).(nome metodo)(eventuali argomenti....);
+	    p3.setPrezzo(190.50);
+	    
+	}
+}
 
